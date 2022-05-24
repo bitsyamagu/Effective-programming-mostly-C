92b9c441f05f5e3ff4588e6c1432550b8480fff6
yamagu <dyamaguc@bits.cc>
2022-05-24 17:46:46 +0900

    example of bulk memory allocation

---
+++ fastq_02/main2_bulk.c
@@ -0,0 +1,47 @@
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct {
+    char name[256];
+    char seq[256];
+    char qual[256];
+} FastQ;
+
+int main(int argc, char** argv){
+    FILE* fp = fopen(argv[1], "r");
+    char buf[256];
+    FastQ* list = (FastQ*)malloc(sizeof(FastQ*)*12);
+    int index = 0;
+    while(1) {
+        // name
+        char* p = fgets(buf, 256, fp);
+        if(p == NULL){
+            break;
+        }
+        strcpy(list[index].name, buf);
+        // seq
+        fgets(buf, 256, fp);
+        strcpy(list[index].seq, buf);
+        // +
+        fgets(buf, 256, fp);
+        // qual
+        fgets(buf, 256, fp);
+        strcpy(list[index].qual, buf);
+
+        index++;
+    }
+
+    for(int i = 0; i<12; i++){
+        printf("%s", list[i].name);
+        printf("%s", list[i].seq);
+        printf("+\n");
+        printf("%s", list[i].qual);
+    }
+    
+    for(int i = 0; i<index; i++){
+        free(list);
+    }
+
+    return 0;
+}
